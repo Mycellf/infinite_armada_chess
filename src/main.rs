b18a3d7f447b90b7da89d872d4213913b11741ff
jeffrey <jcouch210@gmail.com>
2025-03-29 14:21:06 -0800

    Add support for different screen hights

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,13 @@ use macroquad::{
 
 #[macroquad::main("Infinite Armada Chess")]
 async fn main() {
-    const SCREEN_HEIGHT: f32 = 8.0;
+    const SCREEN_HEIGHT: f32 = 9.5;
+    const SCREEN_START_POSITION: f32 = -(SCREEN_HEIGHT - 8.0) / 2.0;
 
     let mut world_camera = Camera2D {
         zoom: [1.0, -2.0 / SCREEN_HEIGHT].into(),
         offset: [0.0, -1.0].into(),
-        target: [ChessBoard::RANK_WIDTH / 2.0, 0.0].into(),
+        target: [ChessBoard::RANK_WIDTH / 2.0, SCREEN_START_POSITION].into(),
         ..Default::default()
     };
 
@@ -39,12 +40,12 @@ async fn main() {
         world_camera.target.y += input as f32 * speed * time::get_frame_time();
 
         if input::is_key_pressed(KeyCode::C) {
-            world_camera.target.y = 0.0;
+            world_camera.target.y = SCREEN_START_POSITION;
         }
 
         if input::is_key_pressed(KeyCode::Space) {
             board.turn = board.turn.opposite();
-            world_camera.target.y = -world_camera.target.y;
+            world_camera.target.y = -world_camera.target.y + 2.0 * SCREEN_START_POSITION;
         }
 
         update_camera_aspect_ratio(&mut world_camera);
